8211377841f61dd39ded64625fd22d0afd3b6e85
Emmanuel dos Santos <manel.pb@gmail.com>
2019-07-31 11:05:49 -0400

    add 3.10

---
+++ 3.10/statistics_nodes_node_list.go
@@ -0,0 +1,28 @@
+// Copyright (C) 2018, Pulse Secure, LLC.
+// Licensed under the terms of the MPL 2.0. See LICENSE file for details.
+
+// Go library for Pulse Virtual Traffic Manager REST version 4.0.
+package vtm
+
+import (
+	"encoding/json"
+)
+
+func (vtm VirtualTrafficManager) ListNodes() (*[]string, *vtmErrorResponse) {
+	conn := vtm.connector.getChildConnector("/tm/3.10/status/local_tm/statistics/nodes/node")
+	data, ok := conn.get()
+	if ok != true {
+		object := new(vtmErrorResponse)
+		json.NewDecoder(data).Decode(object)
+		return nil, object
+	}
+	objectList := new(vtmObjectChildren)
+	if err := json.NewDecoder(data).Decode(objectList); err != nil {
+		panic(err)
+	}
+	var stringList []string
+	for _, obj := range objectList.Children {
+		stringList = append(stringList, obj.Name)
+	}
+	return &stringList, nil
+}
